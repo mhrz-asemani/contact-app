da105dd159e383c4efa6a8170493227619e7f359
amin <aseamin90@gmail.com>
2024-06-14 19:25:04 +0330

    home note

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,18 @@
 const ContactList = props => {
-    console.log(props)
 
-    return <div className="ui celled list">
-        Contact List
-    </div>
+  const renderContactList = props.contacts.map(contact => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">{contact.name}</div>
+          <div className="email">{contact.email}</div>
+        </div>
+        <i className='icon trash alternate outline'></i>
+      </div>
+    );
+  });
+
+  return <div className="ui celled list">{renderContactList}</div>
 }
 
 export default ContactList;
