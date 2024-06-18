5d7e225cca0b98ca7b29f7ea6bd724a931f3cba2
amin <aseamin90@gmail.com>
2024-06-18 15:58:54 +0330

    office notes

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,16 +1,29 @@
 import user from "../images/avatar.jpg";
+import {Link, useLocation} from "react-router-dom";
 
-const ContactDetails = () =>
-  <div className="main">
-    <div className="ui card centered">
-      <div className="image">
-        <img src={user} alt="user"/>
-      </div>
-      <div className="content">
-        <div className="header">Amin</div>
-        <div className="description">Aminoo@gmail.com</div>
-      </div>
-    </div>
-  </div>
+const ContactDetails = () => {
+    const location = useLocation();   // to get props passed to the Link component
+    // check if state is passed
+    const {name, email} = location.state ? location.state : {name: 'name', email: 'email'};
+
+    return (
+        <div className="main">
+            <div className="ui card centered">
+                <div className="image">
+                    <img src={user} alt="user"/>
+                </div>
+                <div className="content">
+                    <div className="header">{name}</div>
+                    <div className="description">{email}</div>
+                </div>
+            </div>
+            <div className="center-div">
+                <Link to="/">
+                    <button className="ui button blue center">Back to Contact List</button>
+                </Link>
+            </div>
+        </div>
+    )
+};
 
 export default ContactDetails;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,7 +3,6 @@ import {Link} from "react-router-dom";
 
 
 const ContactList = props => {
-  console.log(props)
   const renderContactList = props.contacts.map(contact =>
     <ContactCard
       contact={contact}
